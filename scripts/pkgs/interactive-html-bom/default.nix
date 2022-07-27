1dbb996a8492acd40d41b6819a48f212bb67bdf1
Richard Goulter <richard.goulter@gmail.com>
2022-07-27 10:14:33 +0700

    wip; trying to get ibom working again

--- scripts/pkgs/interactive-html-bom/default.nix
+++ scripts/pkgs/interactive-html-bom/default.nix
@@ -1,23 +1,20 @@
 { stdenv
 , fetchFromGitHub
 , python3
-, writeScriptBin
-, kicad
+, writeShellApplication
+, virtualgl
 }:
 
 let
-  kicadPythonModule = toPythonModule (kicad.override {
-    inherit python3;
-  }).src;
   interactive-html-bom = stdenv.mkDerivation rec {
       pname = "interactive-html-bom";
       version = "2.4.1";
 
       src = fetchFromGitHub {
         owner = "openscopeproject";
-        repo = pname;
+        repo = "InteractiveHtmlBom";
         rev = "v${version}";
-        sha256 = "09c2q9735jsy5q1ny6rkr757vvg6nr36ix9hslb78yh9r5jlg6rb";
+        sha256 = "sha256-K5tHZckJenQW1TD1aEa25u19yskzG28DLl7LMk7CgiU=";
       };
 
       installPhase = ''
@@ -28,10 +25,17 @@ let
       patches = [./remove-footprint-from-default-group-field.patch];
     };
   python-with-my-packages = python3.withPackages (p: with p; [
-    kicadPythonModule
+    kicad
     wxPython_4_1
   ]);
 in
-writeScriptBin "InteractiveHtmlBom" ''
-${python-with-my-packages}/bin/python ${interactive-html-bom}/InteractiveHtmlBom/generate_interactive_bom.py $*
-''
+writeShellApplication {
+  name = "InteractiveHtmlBom";
+  text = ''
+    vglrun -d :1 ${python-with-my-packages}/bin/python ${interactive-html-bom}/InteractiveHtmlBom/generate_interactive_bom.py $*
+  '';
+  runtimeInputs = [
+    virtualgl
+  ];
+  checkPhase = "";
+}
